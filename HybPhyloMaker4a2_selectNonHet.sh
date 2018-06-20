f6d05c83e4ebed92b6c4e021f5527085845c02d3
Tomas Fer <tomas.fer@centrum.cz>
2018-06-20 15:37:14 +0200

    v.1.6.4.

--- HybPhyloMaker4a2_selectNonHet.sh
+++ HybPhyloMaker4a2_selectNonHet.sh
@@ -20,8 +20,8 @@
 # ********************************************************************************
 # *    HybPhyloMaker - Pipeline for Hyb-Seq data processing and tree building    *
 # *                  https://github.com/tomas-fer/HybPhyloMaker                  *
-# *                  Script 04a2 - Select non-heterozygous exons                 *
-# *                                   v.1.6.2                                    *
+# *                 Script 04a2 - Select low-heterozygosity exons                *
+# *                                   v.1.6.4                                    *
 # * Tomas Fer, Dept. of Botany, Charles University, Prague, Czech Republic, 2018 *
 # * tomas.fer@natur.cuni.cz                                                      *
 # * based on Weitemier et al. (2014), Applications in Plant Science 2(9): 1400042*
@@ -92,11 +92,9 @@ fi
 if [[ $cp =~ "yes" ]]; then
 	echo -e "Working with cpDNA\n"
 	type="cp"
-	cp $source/$cpDNACDS .
 else
 	echo -e "Working with exons\n"
 	type="exons"
-	cp $source/$probes .
 fi
 
 #Check necessary files
@@ -150,13 +148,19 @@ if [ ! -d "$path/$type/60mafft" ]; then
 	exit 3
 else
 	if [ -d "$path/$type/70concatenated_exon_alignments_NoHet" ]; then
-		echo -e "Directory '$path/$type/70concatenated_exon_alignments_NoHet' already exists. Delete it or rename before running this script again. Exiting...\n"
-		rm -d ../workdir04a2/ 2>/dev/null
-		exit 3
+		if [[ $nohetcalculation =~ "yes" ]]; then
+			echo -e "Directory '$path/$type/70concatenated_exon_alignments_NoHet' already exists. Delete it or rename before running this script again. Exiting...\n"
+			rm -d ../workdir04a2/ 2>/dev/null
+			exit 3
+		elif [ -d "$path/$type/70concatenated_exon_alignments_NoHet/${maxhet}" ]; then
+			echo -e "Directory '$path/$type/70concatenated_exon_alignments_NoHet/${maxhet}' already exists. Delete it or rename before running this script again. Exiting...\n"
+			rm -d ../workdir04a2/ 2>/dev/null
+			exit 3
+		fi
 	else
 		if [[ ! $location == "1" ]]; then
-			if [ "$(ls -A ../workdir04a)" ]; then
-				echo -e "Directory 'workdir04a' already exists. Delete it or rename before running this script again. Exiting...\n"
+			if [ "$(ls -A ../workdir04a2)" ]; then
+				echo -e "Directory 'workdir04a2' already exists. Delete it or rename before running this script again. Exiting...\n"
 				rm -d ../workdir04a2/ 2>/dev/null
 				exit 3
 			fi
@@ -164,96 +168,116 @@ else
 	fi
 fi
 
-# Make new folder for results
-mkdir $path/$type/70concatenated_exon_alignments_NoHet
-mkdir $path/$type/70concatenated_exon_alignments_NoHet/summary
-
-#--------------VARIANT CALLING AND CALCULATION OF NUMBER OF HETEROZYGOUS SITES PER EXON---------------
-# Cut filename before first '.', i.e. remove suffix - does not work if there are other dots in reference file name
-if [[ $cp =~ "yes" ]]; then
-	name=`ls $cpDNACDS | cut -d'.' -f 1`
-else
-	name=`ls $probes | cut -d'.' -f 1`
+#Test if summary of heterozygous sites already exists
+if [[ $nohetcalculation =~ "no" ]] && [ ! -f "$path/$type/70concatenated_exon_alignments_NoHet/summary/summary.txt" ]; then
+	echo -e "Summary file does not exists. Run this script with 'nohetcalculation=yes' in order to create it. Exiting...\n"
+	rm -d ../workdir04a2/ 2>/dev/null
+	exit 3
 fi
-reference=${name}_with${nrns}Ns_beginend.fas
-cp $source/$reference .
-header=$(grep ">" ${name}_with${nrns}Ns_beginend.fas | sed 's/>//')
-bedfile=${header}_exon_positions.bed
-cp $source/$bedfile .
-# Get exon positions
-awk '{ print $2 "\t" $3}' $bedfile > positions.txt
-# Get exon names
-grep ">" $probes > summary.txt
-paste summary.txt positions.txt > tmp && mv tmp summary.txt
-#add as first line
-sed -i "1i exon\tFrom\tTo" summary.txt
-sed -i 's/>//' summary.txt
-cp summary.txt commoncolumns.txt
-# Copy BAM files
-cp $path/$type/21mapped_${mappingmethod}/*.bam .
-#indexing reference
-bwa index $reference
-# Sorting, indexing, variant calling, extraxcting heteroyzgous positions, calculating nr hetero per exon
-for file in $(ls *.bam | cut -d'.' -f1); do
-	echo -e "$file"
-	echo -e "...sorting"
-	samtools sort ${file}.bam -o ${file}_sorted.bam
-	rm ${file}.bam
-	echo -e "...indexing"
-	samtools index ${file}_sorted.bam
-	#samtools view -h -o ${file}.sam ${file}_sorted.bam
-	echo -e "...pileup"
-	samtools mpileup -E -uf $reference ${file}_sorted.bam > ${file}.pileup
-	echo -e "...vcf"
-	bcftools call --skip-variants indels --multiallelic-caller --variants-only -O v -o ${file}.vcf ${file}.pileup
-	#bcftools view -v -m 0.5 ${file}.pileup > ${file}.vcf
-	rm ${file}.pileup
-	#grep heterozygous
-	echo -e "...grep hetero"
-	grep -e 0/1 -e 1/2 ${file}.vcf > ${file}_hetero.txt
-	#only SNPs with mapping quality higher than 36 (mapping quality in 6th column)
-	awk '{if ($6 >= 36) print $0}' ${file}_hetero.txt > ${file}_heteroqual.txt
-	echo -e "...calculate nr het per exon"
-	echo -e "$file" > ${file}_nrhet.txt
-	cat $bedfile | while read a b c; do
-		nrhet=$(awk -v low=$b -v high=$c '{ if (($2 >= low) && ($2 <= high)) print $0 }' ${file}_heteroqual.txt | wc -l)
-		echo -e "$nrhet" >> ${file}_nrhet.txt
+
+#Setting for the case when working with cpDNA
+if [[ $cp =~ "yes" ]]; then
+	cp $source/$cpDNACDS .
+else
+	cp $source/$probes .
+fi
+
+# Make new folder for results
+mkdir -p $path/$type/70concatenated_exon_alignments_NoHet
+mkdir -p $path/$type/70concatenated_exon_alignments_NoHet/summary
+mkdir -p $path/$type/70concatenated_exon_alignments_NoHet/${maxhet}
+
+if [[ $nohetcalculation =~ "yes" ]]; then
+	#--------------VARIANT CALLING AND CALCULATION OF NUMBER OF HETEROZYGOUS SITES PER EXON---------------
+	# Cut filename before first '.', i.e. remove suffix - does not work if there are other dots in reference file name
+	if [[ $cp =~ "yes" ]]; then
+		name=`ls $cpDNACDS | cut -d'.' -f 1`
+	else
+		name=`ls $probes | cut -d'.' -f 1`
+	fi
+	reference=${name}_with${nrns}Ns_beginend.fas
+	cp $source/$reference .
+	header=$(grep ">" ${name}_with${nrns}Ns_beginend.fas | sed 's/>//')
+	bedfile=${header}_exon_positions.bed
+	cp $source/$bedfile .
+	# Get exon positions
+	awk '{ print $2 "\t" $3}' $bedfile > positions.txt
+	# Get exon names
+	grep ">" $probes > summary.txt
+	paste summary.txt positions.txt > tmp && mv tmp summary.txt
+	#add as first line
+	sed -i "1i exon\tFrom\tTo" summary.txt
+	sed -i 's/>//' summary.txt
+	cp summary.txt commoncolumns.txt
+	# Copy BAM files
+	cp $path/$type/21mapped_${mappingmethod}/*.bam .
+	#indexing reference
+	bwa index $reference
+	# Sorting, indexing, variant calling, extraxcting heteroyzgous positions, calculating nr hetero per exon
+	for file in $(ls *.bam | cut -d'.' -f1); do
+		echo -e "$file"
+		echo -e "...sorting"
+		samtools sort ${file}.bam -o ${file}_sorted.bam
+		rm ${file}.bam
+		echo -e "...indexing"
+		samtools index ${file}_sorted.bam
+		#samtools view -h -o ${file}.sam ${file}_sorted.bam
+		echo -e "...pileup"
+		samtools mpileup -E -uf $reference ${file}_sorted.bam > ${file}.pileup
+		echo -e "...vcf"
+		bcftools call --skip-variants indels --multiallelic-caller --variants-only -O v -o ${file}.vcf ${file}.pileup
+		#bcftools view -v -m 0.5 ${file}.pileup > ${file}.vcf
+		rm ${file}.pileup
+		#grep heterozygous
+		echo -e "...grep hetero"
+		grep -e 0/1 -e 1/2 ${file}.vcf > ${file}_hetero.txt
+		#only SNPs with mapping quality higher than 36 (mapping quality in 6th column)
+		awk '{if ($6 >= 36) print $0}' ${file}_hetero.txt > ${file}_heteroqual.txt
+		echo -e "...calculate nr het per exon"
+		echo -e "$file" > ${file}_nrhet.txt
+		cat $bedfile | while read a b c; do
+			nrhet=$(awk -v low=$b -v high=$c '{ if (($2 >= low) && ($2 <= high)) print $0 }' ${file}_heteroqual.txt | wc -l)
+			echo -e "$nrhet" >> ${file}_nrhet.txt
+		done
+		paste commoncolumns.txt ${file}_nrhet.txt > ${file}_nrhet_per_exon.txt
+		cat ${file}_nrhet.txt | cut -f3 > ${file}_nrhetonly.txt
+		paste summary.txt ${file}_nrhetonly.txt > tmp && mv tmp summary.txt
+		rm ${file}_nrhetonly.txt ${file}_nrhet.txt
+		sed -i 's/>//' ${file}_nrhet_per_exon.txt
+		cp ${file}_nrhet_per_exon.txt $path/$type/70concatenated_exon_alignments_NoHet/summary
+		#only exons with maximum specified heterozygosity
+		awk -v val=${maxhet} '$4 <= val' ${file}_nrhet_per_exon.txt | cut -d'_' -f1,2 | sort | uniq > ${file}_ExonsWithoutHet${maxhet}.txt
+		cp ${file}_ExonsWithoutHet${maxhet}.txt $path/$type/70concatenated_exon_alignments_NoHet/summary
 	done
-	paste commoncolumns.txt ${file}_nrhet.txt > ${file}_nrhet_per_exon.txt
-	cat ${file}_nrhet.txt | cut -f3 > ${file}_nrhetonly.txt
-	paste summary.txt ${file}_nrhetonly.txt > tmp && mv tmp summary.txt
-	rm ${file}_nrhetonly.txt ${file}_nrhet.txt
-	sed -i 's/>//' ${file}_nrhet_per_exon.txt
-	#only exons with zero heterozygosity
-	awk '$4 == 0' ${file}_nrhet_per_exon.txt | cut -d'_' -f1,2 | sort | uniq > ${file}_ExonsWithoutHet.txt
-	cp ${file}_nrhet_per_exon.txt $path/$type/70concatenated_exon_alignments_NoHet/summary
-	cp ${file}_ExonsWithoutHet.txt $path/$type/70concatenated_exon_alignments_NoHet/summary
-done
+	
+	#extract first line (header) and add two columns to the end 'total' and 'max'
+	head -n1 summary.txt | awk '{ print $0 "\t" "total" "\t" "max" }' > headerSummary.txt
+	sed -i '1d' summary.txt # delete first line
+	# Compute sum from numbers on each line (omitting first three columns with assembly name, start and stop positions), i.e. sum of heterozygous sites over all samples,
+	# and add this as a last value on each line (last sed command replaces double spaces by TABs)
+	awk '
+	BEGIN {FS=OFS=" "}
+	{
+	sum=0; n=0
+	for(i=4;i<=NF;i++)
+		 {sum+=$i; ++n}
+		 print $0," "sum
+		 sum=0; n=0
+	}' summary.txt | sed "s/  /\t/" > tmp && mv tmp summary.txt
+	#Add highest number of heterozygous sites per locus over samples as the last column in 'summary.txt'
+	awk '{max=$4;for(i=4;i<=NF-1;i++){if($i > max) max = $i}print $0 "\t"max}' summary.txt > tmp && mv tmp summary.txt
+	cat headerSummary.txt summary.txt > tmp && mv tmp summary.txt
+	cp summary.txt $path/$type/70concatenated_exon_alignments_NoHet/summary
+else
+	cp $path/$type/70concatenated_exon_alignments_NoHet/summary/summary.txt .
+fi
 
-#-----------------------CREATE LIST OF NON-HETEROZYGOUS EXONS OVER ALL SAMPLES-----------------------
-# extract first line (header) and add last column 'total'
-head -n1 summary.txt | awk '{ print $0 "\t" "total" }' > headerSummary.txt
-sed -i '1d' summary.txt # delete first line
-# Compute sum from numbers on each line (omitting first three columns with assembly name, start and stop positions), i.e. sum of heterozygous sites over all samples,
-# and add this as a last value on each line (last sed command replaces double spaces by TABs)
-awk '
-BEGIN {FS=OFS=" "}
-{
-sum=0; n=0
-for(i=4;i<=NF;i++)
-     {sum+=$i; ++n}
-     print $0," "sum
-	 sum=0; n=0
-}' summary.txt | sed "s/  /\t/" > tmp && mv tmp summary.txt
-
-awk '$NF == 0' summary.txt | awk '{ print $1 }' | sort | uniq > exonsWithoutHet.txt
-
-cat headerSummary.txt summary.txt > tmp && mv tmp summary.txt
-cp summary.txt $path/$type/70concatenated_exon_alignments_NoHet/summary
-cp exonsWithoutHet.txt $path/$type/70concatenated_exon_alignments_NoHet/summary
+#-----------CREATE A LIST OF EXONS WITH LIMITED NUMBER OF NON-HETEROZYGOUS SITES OVER ALL SAMPLES-----------
+awk -v val=${maxhet} '$NF <= val { print $1 }' summary.txt | sort | uniq > exonsWithoutHet${maxhet}.txt
+cp exonsWithoutHet${maxhet}.txt $path/$type/70concatenated_exon_alignments_NoHet/${maxhet}
 
 #------------------------------COPY MAFFT FILES-------------------------------
-for i in $(cat exonsWithoutHet.txt); do
+for i in $(cat exonsWithoutHet${maxhet}.txt); do
 	cp $path/$type/60mafft/To_align_${i}.fasta.mafft .
 done
 
@@ -313,7 +337,7 @@ if [[ $cp =~ "no" ]]; then
 		mv partitions.txt ${b}.part
 		#perl catfasta2phyml.pl -f $a > $b.fasta
 		#perl catfasta2phyml.pl $b.fasta > $b.phylip
-		cp $b.* $path/$type/70concatenated_exon_alignments_NoHet
+		cp $b.* $path/$type/70concatenated_exon_alignments_NoHet/${maxhet}
 	done
 	echo -e "finished"
 fi
