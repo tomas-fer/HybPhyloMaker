e5297f96d55d0880017571f2651286502e43316c
Tomas Fer <tomas.fer@centrum.cz>
2020-05-28 15:22:25 +0200

    v.1.7.0.

---
+++ HybPhyloMaker11_phyparts.sh
@@ -0,0 +1,315 @@
+#!/bin/bash
+#----------------MetaCentrum----------------
+#PBS -l walltime=24:0:0
+#PBS -l select=1:ncpus=2:mem=16gb:scratch_local=4gb
+#PBS -j oe
+#PBS -N HybPhyloMaker11_PhyParts_Astral
+#PBS -m abe
+#-------------------HYDRA-------------------
+#$ -S /bin/bash
+#$ -pe mthread 4
+#$ -q sThM.q
+#$ -l mres=4G,h_data=4G,h_vmem=4G,himem
+#$ -cwd
+#$ -j y
+#$ -N HybPhyloMaker11_PhyParts_Astral
+#$ -o HybPhyloMaker11_PhyParts_Astral.log
+
+# ********************************************************************************
+# *    HybPhyloMaker - Pipeline for Hyb-Seq data processing and tree building    *
+# *                  https://github.com/tomas-fer/HybPhyloMaker                  *
+# *                 Script 11 - PhyParts for Astral species tree                 *
+# *                                   v.1.7.0                                    *
+# * Tomas Fer, Dept. of Botany, Charles University, Prague, Czech Republic, 2020 *
+# * tomas.fer@natur.cuni.cz                                                      *
+# ********************************************************************************
+
+
+#Runs PhyParts and PhyParts PieCharts for ASTRAL tree
+#Takes all selected gene trees and only select trees with outgroup
+
+#Complete path and set configuration for selected location
+if [[ $PBS_O_HOST == *".cz" ]]; then
+	echo -e "\nHybPhyloMaker11_PhyParts is running on MetaCentrum..."
+	#settings for MetaCentrum
+	#Move to scratch
+	cd $SCRATCHDIR
+	#Copy file with settings from home and set variables from settings.cfg
+	cp $PBS_O_WORKDIR/settings.cfg .
+	. settings.cfg
+	#. /packages/run/modules-2.0/init/bash
+	path=/storage/$server/home/$LOGNAME/$data
+	source=/storage/$server/home/$LOGNAME/HybSeqSource
+	#Add necessary modules
+	module add phyparts-0.0.1
+	module add newick-utils-13042016
+elif [[ $HOSTNAME == compute-*-*.local ]]; then
+	echo -e "\nHybPhyloMaker11_PhyParts is running on Hydra..."
+	#settings for Hydra
+	#set variables from settings.cfg
+	. settings.cfg
+	path=../$data
+	source=../HybSeqSource
+	#Make and enter work directory
+	mkdir -p workdir11
+	cd workdir11
+	#Add necessary modules
+	module load bioinformatics/anaconda3/5.1 #adds NewickUtilities
+	module load ???phyparts#???
+	echo "PhyParts not yet installed on Hydra, exiting..."
+	exit 3
+else
+	echo -e "\nHybPhyloMaker11_PhyParts is running locally..."
+	#settings for local run
+	#set variables from settings.cfg
+	. settings.cfg
+	path=../$data
+	source=../HybSeqSource
+	#Make and enter work directory
+	mkdir -p workdir11
+	cd workdir11
+fi
+
+#Setting for the case when working with cpDNA
+if [[ $cp =~ "yes" ]]; then
+	echo -en "Working with cpDNA"
+	type="cp"
+else
+	echo -en "Working with exons"
+	type="exons"
+fi
+
+#Settings for (un)corrected reading frame
+if [[ $corrected =~ "yes" ]]; then
+	alnpath=$type/80concatenated_exon_alignments_corrected
+	alnpathselected=$type/81selected_corrected
+	treepath=$type/82trees_corrected
+	echo -en "...with corrected reading frame"
+else
+	alnpath=$type/70concatenated_exon_alignments
+	alnpathselected=$type/71selected
+	treepath=$type/72trees
+fi
+
+if [[ $update =~ "yes" ]]; then
+	echo -e "...and with updated gene selection"
+else
+	echo -e ""
+fi
+
+if [[ ! $collapse -eq "0" ]]; then
+	echo -e "...and with trees with branches below ${collapse} BS collapsed"
+else
+	if [[ $requisite =~ "no" ]]; then
+		echo -e "\n"
+	fi
+fi
+
+if [[ $requisite =~ "yes" ]]; then
+	echo -e "...and only with trees with requisite taxa present\n"
+else
+	echo -e "\n"
+fi
+
+#Settings for collapsed and requisite selection
+if [[ $requisite =~ "yes" ]]; then
+	if [[ ! $collapse -eq "0" ]]; then
+		modif=with_requisite/collapsed${collapse}/
+		treefile=trees_with_requisite_collapsed${collapse}.newick
+	else
+		modif=with_requisite/
+		if [ -z "$OUTGROUP" ]; then
+			treefile=trees_with_requisite.newick
+		else
+			treefile=trees_rooted_with_requisite.newick
+		fi
+	fi
+else
+	if [[ ! $collapse -eq "0" ]]; then
+		modif=collapsed${collapse}/
+		treefile=trees_collapsed${collapse}.newick
+	else
+		modif=""
+		if [ -z "$OUTGROUP" ]; then
+			treefile=trees.newick
+		else
+			treefile=trees_rooted.newick
+		fi
+	fi
+fi
+
+#Check if $OUTGROUP is set
+if [ -z $OUTGROUP ]; then
+	echo -e "Outgroup is not set. Exiting..."
+	rm -d ../workdir11 2>/dev/null
+	exit 3
+fi
+
+#Define Astral tree name
+if [[ $requisite =~ "yes" ]]; then
+	if [[ ! $collapse -eq "0" ]]; then
+		astraltree=Astral_${MISSINGPERCENT}_${SPECIESPRESENCE}_with_requisite_collapsed${collapse}.tre
+	else
+		astraltree=Astral_${MISSINGPERCENT}_${SPECIESPRESENCE}_with_requisite.tre
+	fi
+else
+	if [[ ! $collapse -eq "0" ]]; then
+		astraltree=Astral_${MISSINGPERCENT}_${SPECIESPRESENCE}_collapsed${collapse}.tre
+	else
+		astraltree=Astral_${MISSINGPERCENT}_${SPECIESPRESENCE}.tre
+	fi
+fi
+
+#Remove '.tre' from species tree name
+sptree=$(cut -d'.' -f1 <<< $astraltree)
+
+#Copy species tree
+echo -e "\nCopying trees..."
+if [[ $update =~ "yes" ]]; then
+	cp $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral/${astraltree} .
+else
+	cp $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral/${astraltree} .
+fi
+
+#Copy gene trees (file with all gene trees)
+if [[ $update =~ "yes" ]]; then
+	if [ -z "$OUTGROUP" ]; then
+		cp $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${treefile} .
+	else
+		cp $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${treefile} .
+	fi
+else
+	if [ -z "$OUTGROUP" ]; then
+		cp $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}${treefile} .
+	else
+		cp $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}${treefile} .
+	fi
+fi
+
+#Modify Astral tree (replace ' ' back to '-' and '_'
+sed -i 's/ \([^ ]*\) / \1_/g' ${sptree}.tre #replace every second occurrence of ' ' by '_'
+sed -i 's/ /-/g' ${sptree}.tre #replace all spaces by '-'
+
+#Modify labels in gene tree
+sed -i.bak 's/XX/-/g' $treefile
+sed -i.bak2 's/YY/_/g' $treefile
+
+#Get only rooted gene trees (i.e., trees containing outgroup)
+nrgenetreesorig=$(wc -l < $treefile)
+grep "$OUTGROUP" ${treefile} > tmp
+mv tmp ${treefile}
+
+#Check if there are any gene trees left
+if [ $(wc -l < ${treefile}) -eq 0 ]; then
+	echo -e "\nThere are no gene trees rooted with ${OUTGROUP}. Exiting..."
+	exit 3
+fi
+
+#Reroot genestrees with $OUTGROUP
+nw_reroot -s ${treefile} $OUTGROUP > tmp && mv tmp ${treefile}
+
+#Put single tree per file to directory 'trees'
+mkdir trees
+split -a 4 -d -l 1 ${treefile} trees/tree_
+
+nrgenetreesrooted=$(ls trees/tree_* | wc -l)
+
+#Subselect only first 'nrpptrees' if necessary (to decrease running time)
+if [ -z "$nrpptrees" ]; then #test whether $nrpptrees is empty
+	nrpptrees=$nrgenetreesrooted #if empty set to number of rooted gene trees
+fi
+
+if [ $nrpptrees -gt $nrgenetreesrooted ]; then #test whether $nrpptrees is higher than nr. of rooted trees
+	nrpptrees=$nrgenetreesrooted #if higher set to number of rooted gene trees
+fi
+
+if [ $nrpptrees -lt $nrgenetreesrooted ]; then
+	echo -e "\nSubselecting $nrpptrees trees..."
+	mkdir trees$nrpptrees
+	cd trees
+	ls tree_* | head -n $nrpptrees | xargs -I{} cp "{}" ../trees$nrpptrees/
+	cd ..
+	rm -r trees
+	mv trees$nrpptrees trees
+fi
+
+#Calculate number of gene trees
+nrgenetrees=$(ls trees/tree_* | wc -l)
+
+#Statisctics
+echo -e "\nSpecies tree: ${sptree}.tre" | tee -a phypartsinfo_BS${phypartsbs}_${nrpptrees}trees.txt
+echo -e "Gene tree file: ${treefile}" | tee -a phypartsinfo_BS${phypartsbs}_${nrpptrees}trees.txt
+echo -e "Nr. gene trees: ${nrgenetreesorig}" | tee -a phypartsinfo_BS${phypartsbs}_${nrpptrees}trees.txt
+echo -e "Nr. gene trees rooted with $OUTGROUP: ${nrgenetreesrooted}" | tee -a phypartsinfo_BS${phypartsbs}_${nrpptrees}trees.txt
+echo -e "Using $nrpptrees gene trees" | tee -a phypartsinfo_BS${phypartsbs}_${nrpptrees}trees.txt
+
+#Set to 0.5 if $phypartsbs is empty
+if [ -z "$phypartsbs" ]; then #test whether $phypartsbs is empty
+	echo -e "\n'phypartsbs' is not set to any value, using 0.5..."
+	phypartsbs=0.5
+fi
+
+#Make dir for results
+if [[ $update =~ "yes" ]]; then
+	mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral/phyparts_${phypartsbs}
+	mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral/phyparts_${phypartsbs}/trees_res_${nrpptrees}trees
+else
+	mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral/phyparts_${phypartsbs}
+	mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral/phyparts_${phypartsbs}/trees_res_${nrpptrees}trees
+fi
+
+#Run phyparts
+echo -e "\nRunning PhyParts with support cutoff ${phypartsbs}..."
+# -a what kind of analysis (0 - concon, 1 - fullconcon, 2 - duplications)
+# -d directory of trees
+# -m  mapping tree (species tree)
+# -o prepend output files with this
+# -s support cutoff (only keep things with greater support than the one specified)
+# -v include verbose output
+echo "phyparts -a 1 -d trees -m ${sptree}.tre -o trees_res -s ${phypartsbs} -v"
+phyparts -a 1 -d trees -m ${sptree}.tre -o trees_res -s ${phypartsbs} -v > phyparts.log 2>&1
+#java -jar /software/phyparts/0.0.1/target/phyparts-0.0.1-SNAPSHOT-jar-with-dependencies.jar -a 1 -d trees -m ${sptree}.tre -o trees_res -s ${phypartsbs} -v > phyparts.log 2>&1
+
+#Run phypartspiecharts
+echo -e "\nRunning PhyParts PieCharts..."
+echo -e "Using colours: ${ppcolors}"
+# --svg_name name of the resulting graphics
+# species tree
+# prefix of phypart results (i.e., the same as '-o' option in phyparts)
+# number of genetrees
+if [ -z "$ppcolors" ]; then
+	echo "phypartspiecharts --svg_name phyparts_${sptree}_BS${phypartsbs}_${nrpptrees}trees.svg ${sptree}.tre trees_res ${nrgenetrees}"
+	phypartspiecharts --svg_name phyparts_${sptree}_BS${phypartsbs}_${nrpptrees}trees.svg ${sptree}.tre trees_res ${nrgenetrees}
+else
+	echo "phypartspiecharts --svg_name phyparts_${sptree}_BS${phypartsbs}_${nrpptrees}trees.svg ${sptree}.tre trees_res ${nrgenetrees} --colors ${ppcolors}"
+	phypartspiecharts --svg_name phyparts_${sptree}_BS${phypartsbs}_${nrpptrees}trees.svg ${sptree}.tre trees_res ${nrgenetrees} --colors ${ppcolors}
+	#python /software/phyparts/0.0.1/target/phypartspiecharts.py --svg_name phyparts_${sptree}_BS${phypartsbs}_${nrpptrees}trees.svg ${sptree}.tre trees_res ${nrgenetrees} --colors ${ppcolors}
+fi
+
+#Copy results to home
+if [[ $update =~ "yes" ]]; then
+	cp phyparts_${sptree}_BS${phypartsbs}_${nrpptrees}trees.svg $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral/phyparts_${phypartsbs}
+	cp phypartsinfo_BS${phypartsbs}_${nrpptrees}trees.txt $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral/phyparts_${phypartsbs}
+	cp trees_res.*node* $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral/phyparts_${phypartsbs}/trees_res_${nrpptrees}trees
+	cp trees_res.*hist* $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral/phyparts_${phypartsbs}/trees_res_${nrpptrees}trees
+	cp phyparts.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral/phyparts_${phypartsbs}/trees_res_${nrpptrees}trees
+else
+	cp phyparts_${sptree}_BS${phypartsbs}_${nrpptrees}trees.svg $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral/phyparts_${phypartsbs}
+	cp phypartsinfo_BS${phypartsbs}_${nrpptrees}trees.txt $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral/phyparts_${phypartsbs}
+	cp trees_res.*node* $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral/phyparts_${phypartsbs}/trees_res_${nrpptrees}trees
+	cp trees_res.*hist* $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral/phyparts_${phypartsbs}/trees_res_${nrpptrees}trees
+	cp phyparts.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral/phyparts_${phypartsbs}/trees_res_${nrpptrees}trees
+fi
+
+#Clean scratch/work directory
+if [[ $PBS_O_HOST == *".cz" ]]; then
+	#delete scratch
+	if [[ ! $SCRATCHDIR == "" ]]; then
+		rm -rf $SCRATCHDIR/*
+	fi
+else
+	cd ..
+	rm -r workdir11
+fi
+
+echo -e "\nScript HybPhyloMaker11 finished...\n"
