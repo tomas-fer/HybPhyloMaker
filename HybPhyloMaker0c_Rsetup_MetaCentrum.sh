1dbf95fd224fa30ac047917875a3aa27d960e212
Tomas Fer <tomas.fer@centrum.cz>
2021-05-24 20:21:29 +0200

    v.1.6.8.

--- HybPhyloMaker0c_Rsetup_MetaCentrum.sh
+++ HybPhyloMaker0c_Rsetup_MetaCentrum.sh
@@ -10,7 +10,7 @@
 # *    HybPhyloMaker - Pipeline for Hyb-Seq data processing and tree building    *
 # *                  https://github.com/tomas-fer/HybPhyloMaker                  *
 # *                 Script 0c - Setup R packages on Metacentrum                  *
-# *                                   v.1.6.7                                    *
+# *                                   v.1.6.8                                    *
 # * Tomas Fer, Dept. of Botany, Charles University, Prague, Czech Republic, 2021 *
 # * tomas.fer@natur.cuni.cz                                                      *
 # ********************************************************************************
@@ -22,6 +22,8 @@
 #seqinr 3.1-3
 #openxlsx 4.0.0
 #phangorn 2.5.5 (this works also with R 3.5 and lower)
+#BiocManager 1.30.14 (for installation of Bioconductor packages like treeio)
+#treeio 1.4.3 (from Bioconductor, this works with R 3.4)
 
 if [[ $PBS_O_HOST == *".cz" ]]; then
 	echo -e "\nScript HybPhyloMaker0c is running on Metacentrum..."
@@ -35,9 +37,12 @@ cd $SCRATCHDIR
 #Copy file with settings from home and set variables from settings.cfg
 cp $PBS_O_WORKDIR/settings.cfg .
 . settings.cfg
-. /packages/run/modules-2.0/init/bash
+
 #Add necessary modules
 module add R-3.4.3-gcc
+
+#Use this command before running R in HybPhyloMaker scripts
+export R_LIBS="/storage/$server/home/$LOGNAME/Rpackages"
 
 #Get ape 3.5 and other R packages
 echo -e "\nDownloading R packages..."
@@ -49,6 +54,9 @@ wget https://cran.r-project.org/src/contrib/Archive/openxlsx/openxlsx_4.0.0.tar.
 wget https://cran.r-project.org/src/contrib/Archive/quadprog/quadprog_1.5-7.tar.gz #required by phangorn
 wget https://cran.r-project.org/src/contrib/Archive/igraph/igraph_1.2.5.tar.gz #required by phangorn
 wget https://cran.r-project.org/src/contrib/fastmatch_1.1-0.tar.gz #required by phangorn
+wget https://cran.r-project.org/src/contrib/Archive/BiocManager/BiocManager_1.30.14.tar.gz #necessary for treeio on Bioconductor
+wget https://cran.r-project.org/src/contrib/Archive/rvcheck/rvcheck_0.1.7.tar.gz #required by treeio
+wget https://cran.r-project.org/src/contrib/Archive/tidytree/tidytree_0.3.3.tar.gz #required by treeio
 
 #Install packages to personal (writable) library
 mkdir -p /storage/$server/home/$LOGNAME/Rpackages
@@ -57,11 +65,13 @@ for package in $(ls *.tar.gz); do
 	R CMD INSTALL $package -l /storage/$server/home/$LOGNAME/Rpackages
 done
 
+#instal 'phangorn'
 wget https://cran.r-project.org/src/contrib/Archive/phangorn/phangorn_2.5.5.tar.gz
-R CMD INSTALL phangorn_2.5.5.tar.gz -l /storage/$server/home/$LOGNAME/Rpackages #this only works once the previous packeges are installed
+R CMD INSTALL phangorn_2.5.5.tar.gz -l /storage/$server/home/$LOGNAME/Rpackages #this only works once the previous packages are installed
 
-#Use this command before running R in HybPhyloMaker scripts
-export R_LIBS="/storage/$server/home/$LOGNAME/Rpackages"
+#instal 'treeio'
+wget https://www.bioconductor.org/packages/3.7/bioc/src/contrib/treeio_1.4.3.tar.gz
+R CMD INSTALL treeio_1.4.3.tar.gz -l /storage/$server/home/$LOGNAME/Rpackages #this only works once the previous packages are installed
 
 #Delete scratch
 if [[ $PBS_O_HOST == *".cz" ]]; then
