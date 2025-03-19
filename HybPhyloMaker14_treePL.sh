d708a59602c8fd69c95b3ef00f8d0994423a1394
Tomas Fer <tomas.fer@centrum.cz>
2025-03-19 20:49:31 +0100

    v.1.8.0f
    Now possible to date Astral4, FastTree, or ExaML tree. Also creates submission files for dating all selected gene trees. Before, it checks whether the gene tree results do exist with one of possible species tree.

--- HybPhyloMaker14_treePL.sh
+++ HybPhyloMaker14_treePL.sh
@@ -19,7 +19,7 @@
 # *    HybPhyloMaker - Pipeline for Hyb-Seq data processing and tree building    *
 # *                  https://github.com/tomas-fer/HybPhyloMaker                  *
 # *                     Script 14 - treePL divergence dating                     *
-# *                                   v.1.8.0e                                   *
+# *                                   v.1.8.0f                                   *
 # * Tomas Fer, Dept. of Botany, Charles University, Prague, Czech Republic, 2025 *
 # * tomas.fer@natur.cuni.cz                                                      *
 # ********************************************************************************
@@ -173,9 +173,9 @@ else
 fi
 
 if [[ ! $location == "1" ]]; then
-	if [ "$(ls -A ../workdir1$)" ]; then
-		echo -e "Directory 'workdir08a' already exists and is not empty. Delete it or rename before running this script again. Exiting...\n"
-		rm -d ../workdir08a 2>/dev/null
+	if [ "$(ls -A ../workdir14)" ]; then
+		echo -e "Directory 'workdir14' already exists and is not empty. Delete it or rename before running this script again. Exiting...\n"
+		rm -d ../workdir14 2>/dev/null
 		exit 3
 	fi
 fi
@@ -219,7 +219,7 @@ echo -e "\nSettings" >> ${logname}.log
 if [[ $PBS_O_HOST == *".cz" ]]; then
 	printf "%-25s %s\n" `echo -e "\nServer:\t$server"` >> ${logname}.log
 fi
-for set in data selection cp corrected update MISSINGPERCENT SPECIESPRESENCE tree FastTreeBoot OUTGROUP collapse requisite; do
+for set in data selection cp corrected update MISSINGPERCENT SPECIESPRESENCE tree FastTreeBoot OUTGROUP collapse requisite tpltree; do
 	printf "%-25s %s\n" `echo -e "${set}:\t" ${!set}` >> ${logname}.log
 done
 
@@ -236,39 +236,95 @@ if [ ! -z "$selection" ]; then
 	echo >> ${logname}.log
 fi
 
-#Create folder for results & Copy species tree to data (now ExaML tree)
+#Set folder
+if [[ $tpltree =~ "ExaML" ]]; then
+	tpltf=concatenatedExaML
+elif [[ $tpltree =~ "Astral4" ]]; then
+	tpltf=Astral4
+elif [[ $tpltree =~ "FastTree" ]]; then
+	tpltf=concatenated
+fi
+
+#Check if the results exist with other species trees
+if [[ $update =~ "yes" ]]; then
+	if [ -d "$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenatedExaML/treePL/bstrees" ]; then
+		tpltb=concatenatedExaML
+		echo -e "...treePL results for bootstrap trees probably found associated with '$tpltb' tree in '$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenatedExaML/treePL/bstrees' folder."
+		echo -e "Check it and proceed with running the script 14b (available treePL results will be used to annotate dated $tpltree species tree)."
+		echo -e "Exiting...\n"
+		exit 3
+	elif [ -d "$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral4/treePL/bstrees" ]; then
+		tpltb=Astral4
+		echo -e "...treePL results for bootstrap trees probably found associated with '$tpltb' tree in '$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral4/treePL/bstrees' folder."
+		echo -e "Check it and proceed with running the script 14b (available treePL results will be used to annotate dated $tpltree species tree)."
+		echo -e "Exiting...\n"
+		exit 3
+	elif [ -d "$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenated/treePL/bstrees" ]; then
+		tpltb=concatenated
+		echo -e "...treePL results for bootstrap trees probably found associated with '$tpltb' tree in '$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenated/treePL/bstrees' folder."
+		echo -e "Check it and proceed with running the script 14b (available treePL results will be used to annotate dated $tpltree species tree)."
+		echo -e "Exiting...\n"
+		exit 3
+	else
+		echo -e "No treePL results for bootstrap trees found. Continue running the script...\n"
+	fi
+else
+	if [ -d "$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenatedExaML/treePL/bstrees" ]; then
+		tpltb=concatenatedExaML
+		echo -e "...treePL results for bootstrap trees probably found associated with '$tpltb' tree in '$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenatedExaML/treePL/bstrees' folder."
+		echo -e "Check it and proceed with running the script 14b (available treePL results will be used to annotate dated $tpltree species tree)."
+		echo -e "Exiting...\n"
+		exit 3
+	elif [ -d "$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral4/treePL/bstrees" ]; then
+		tpltb=Astral4
+		echo -e "...treePL results for bootstrap trees probably found associated with '$tpltb' tree in '$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral4/treePL/bstrees' folder."
+		echo -e "Check it and proceed with running the script 14b (available treePL results will be used to annotate dated $tpltree species tree)."
+		echo -e "Exiting...\n"
+		exit 3
+	elif [ -d "$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenated/treePL/bstrees" ]; then
+		tpltb=concatenated
+		echo -e "...treePL results for bootstrap trees probably found associated with '$tpltb' tree in '$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenated/treePL/bstrees' folder."
+		echo -e "Check it and proceed with running the script 14b (available treePL results will be used to annotate dated $tpltree species tree)."
+		echo -e "Exiting...\n"
+		exit 3
+	else
+		echo -e "No treePL results for bootstrap trees found. Continue running the script...\n"
+	fi
+fi
+
+#Create folder for results & Copy species tree
+echo -e "Creating folder for results...\n"
 if [[ $tpltree =~ "ExaML" ]]; then
 	tplt=ExaML_BestML_${MISSINGPERCENT}_${SPECIESPRESENCE}.tre
 	if [[ $update =~ "yes" ]]; then
-		mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenatedExaML/treePL
+		mkdir -p $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenatedExaML/treePL
 		cp $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenatedExaML/ExaML_BestML_${MISSINGPERCENT}_${SPECIESPRESENCE}.tre .
 	else
-		mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenatedExaML/treePL
+		mkdir -p $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenatedExaML/treePL
 		cp $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenatedExaML/ExaML_BestML_${MISSINGPERCENT}_${SPECIESPRESENCE}.tre .
 	fi
 elif [[ $tpltree =~ "Astral4" ]]; then
 	tplt=Astral4_${MISSINGPERCENT}_${SPECIESPRESENCE}.tre
 	if [[ $update =~ "yes" ]]; then
-		mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral4/treePL
+		mkdir -p $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral4/treePL
 		cp $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral4/Astral4_${MISSINGPERCENT}_${SPECIESPRESENCE}.tre .
 	else
-		mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral4/treePL
+		mkdir -p $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral4/treePL
 		cp $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral4/Astral4_${MISSINGPERCENT}_${SPECIESPRESENCE}.tre .
 	fi
 elif [[ $tpltree =~ "FastTree" ]]; then
 	tplt=concatenated${MISSINGPERCENT}_${SPECIESPRESENCE}.fast.tre
 	if [[ $update =~ "yes" ]]; then
-		mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenated/treePL
+		mkdir -p $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenated/treePL
 		cp $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenated/concatenated${MISSINGPERCENT}_${SPECIESPRESENCE}.fast.tre .
 	else
-		mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenated/treePL
+		mkdir -p $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenated/treePL
 		cp $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenated/concatenated${MISSINGPERCENT}_${SPECIESPRESENCE}.fast.tre .
 	fi
 else
 	echo -e "'tpltree=' has to be one of ExaML, Astral4 or FastTree. Exiting...\n"
 	exit 3
 fi
-
 
 if [[ $tpltree =~ "Astral4" ]]; then
 	#Modify Astral4 tree (replace ' ' back to '-' and '_')
@@ -283,55 +339,119 @@ fi
 #Root tree
 nw_reroot -s ${tplt} $OUTGROUP > tmp && mv tmp ${tplt}
 
-
-# Run PLtree 
-chmod 755 treepl_wrapper.sh
-./treepl_wrapper.sh configuration.txt ${tplt} treePLresult > treePL_${tpltree}.log
-
-#Modify names
-mv treepl_treePLresult.tre ${tpltree}_treePLresult.tre
-mv treepl_treePLresult.tre.r8s ${tpltree}_treePLresult.tre.r8s
-mv out_dates.tre.r8s ${tpltree}_dates.tre.r8s
-mv out_dates.tre ${tpltree}_dates.tre
-
-#Delete some files 
-rm ${tplt} treepl_wrapper.sh 
-
-#Finish log
-echo -e "\nEnd:" `date '+%A %d-%m-%Y %X'` >> ${logname}.log
-
-# Copy results to home 
-if [[ $tpltree =~ "ExaML" ]]; then
+#Preapare for running treePL on bootstrap trees
+if [[ $tplbs =~ "yes" ]]; then
+	#Check if treePL bootstrap analysis already exists
 	if [[ $update =~ "yes" ]]; then
-		cp * $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenatedExaML/treePL
-		cp ${logname}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenatedExaML/treePL
-		cp treePL_${tpltree}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenatedExaML/treePL
+		if [ -d "$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${tpltf}/treePL/bstrees" ]; then
+			echo -e "The folder '$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${tpltf}/treePL/bstrees' already exists. Delete it or rename before running this script again. Exiting..."
+			exit 3
+		fi
 	else
-		cp * $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenatedExaML/treePL
-		cp ${logname}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenatedExaML/treePL
-		cp treePL_${tpltree}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenatedExaML/treePL
+		if [ -d "$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}${tpltf}/treePL/bstrees" ]; then
+			echo -e "The folder '$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}${tpltf}/treePL/bstrees' already exists. Delete it or rename before running this script again. Exiting..."
+			exit 3
+		fi
 	fi
-elif [[ $tpltree =~ "Astral4" ]]; then
+	#Copy gene tree file & root trees with OUTGROUP
+	echo -e "Copying gene trees & separating & creating jobs...\n"
 	if [[ $update =~ "yes" ]]; then
-		cp * $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral4/treePL
-		cp ${logname}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral4/treePL
-		cp treePL_${tpltree}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}Astral4/treePL
+		cp $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${treefile} .
 	else
-		cp * $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral4/treePL
-		cp ${logname}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral4/treePL
-		cp treePL_${tpltree}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}Astral4/treePL
+		cp $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}${treefile} .
 	fi
-elif [[ $tpltree =~ "FastTree" ]]; then
+	#Only gene trees with OUTGROUP
+	grep ${OUTGROUP} ${treefile} > tmp && mv tmp ${treefile}
+	nw_reroot -s ${treefile} $OUTGROUP > bstrees
+	rm ${treefile}
+	#Split trees to single files (each line to separate file)
+	awk '{filename = sprintf("tree%d.tre", NR); print >filename; close(filename)}' bstrees
+	#Create individual folders, copy input files there
 	if [[ $update =~ "yes" ]]; then
-		cp * $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenated/treePL
-		cp ${logname}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenated/treePL
-		cp treePL_${tpltree}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}concatenated/treePL
+		mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${tpltf}/treePL/bstrees
 	else
-		cp * $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenated/treePL
-		cp ${logname}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenated/treePL
-		cp treePL_${tpltree}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}concatenated/treePL
+		mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}${tpltf}/treePL/bstrees
+	fi
+	for i in $(ls tree*.tre | cut -d'.' -f1); do
+		if [[ $update =~ "yes" ]]; then
+			mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${tpltf}/treePL/bstrees/${i}
+			cp ${i}.tre $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${tpltf}/treePL/bstrees/${i}
+		else
+			mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}${tpltf}/treePL/bstrees/${i}
+			cp ${i}.tre $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}${tpltf}/treePL/bstrees/${i}
+		fi
+	done
+	#Prepare job files
+	touch $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${tpltf}/treePL/bstrees/submitTreePLjobs.sh
+	for group in $(ls tree*.tre | cut -d'.' -f1); do
+		echo '#!/bin/bash' >> ${group}.sh
+		echo '#----------------MetaCentrum----------------' >> ${group}.sh
+		echo '#PBS -l walltime=24:00:00' >> ${group}.sh
+		echo '#PBS -l select=1:ncpus=4:mem=1gb:scratch_local=1gb' >> ${group}.sh
+		echo '#PBS -j oe' >> ${group}.sh
+		echo '#PBS -o /storage/'"$server/home/$LOGNAME" >> ${group}.sh
+		echo '#PBS -N treePL_for_'"${group}" >> ${group}.sh
+		echo '#Add necessary modules' >> ${group}.sh
+		echo 'module add treepl-1.0' >> ${group}.sh
+		echo 'cd $SCRATCHDIR' >> ${group}.sh
+		echo 'cp '"$source"'/treepl_wrapper.sh .' >> ${group}.sh
+		echo 'cp '"$source"'/configuration.txt .' >> ${group}.sh
+		echo 'chmod +x treepl_wrapper.sh' >> ${group}.sh
+		echo 'cp '"$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}"'/update/species_trees/'"${modif}${tpltf}"'/treePL/bstrees/'"${group}"'/'"${group}"'.tre .' >> ${group}.sh
+		echo '#Run PLtree within treepl_wrapper' >> ${group}.sh
+		echo './treepl_wrapper.sh configuration.txt '"${group}"'.tre '"${group}"' > treePL_'"${group}"'.log' >> ${group}.sh
+		echo '#Copy results to home' >> ${group}.sh
+		echo 'cp configure* '"$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}"'/update/species_trees/'"${modif}${tpltf}"'/treePL/bstrees/'"${group}" >> ${group}.sh
+		echo 'cp cv* '"$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}"'/update/species_trees/'"${modif}${tpltf}"'/treePL/bstrees/'"${group}" >> ${group}.sh
+		echo 'cp out_dates* '"$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}"'/update/species_trees/'"${modif}${tpltf}"'/treePL/bstrees/'"${group}" >> ${group}.sh
+		echo 'cp prime* '"$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}"'/update/species_trees/'"${modif}${tpltf}"'/treePL/bstrees/'"${group}" >> ${group}.sh
+		echo 'cp treepl*.tre* '"$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}"'/update/species_trees/'"${modif}${tpltf}"'/treePL/bstrees/'"${group}" >> ${group}.sh
+		echo 'cp *.log '"$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}"'/update/species_trees/'"${modif}${tpltf}"'/treePL/bstrees/'"${group}" >> ${group}.sh
+		chmod +x ${group}.sh
+		if [[ $update =~ "yes" ]]; then
+			cp ${group}.sh $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${tpltf}/treePL/bstrees/
+			echo 'qsub '"${group}"'.sh' >> $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${tpltf}/treePL/bstrees/submitTreePLjobs.sh
+		else
+			cp ${group}.sh $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}${tpltf}/treePL/bstrees/
+			echo 'qsub '"${group}"'.sh' >> $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}${tpltf}/treePL/bstrees/submitTreePLjobs.sh
+		fi
+	done
+	#Make the submitter executable
+	chmod +x $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${tpltf}/treePL/bstrees/submitTreePLjobs.sh
+fi
+
+#Run main treePL analysis
+if [ -f "$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${tpltf}/treePL/${tpltree}_treePLresult.tre" ]; then
+	echo -e "The file '${tpltree}_treePLresult.tre' already exist in '$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${tpltf}/treePL/'. Delete it or rename before running this script again. No treePL analysis now...\n"
+else
+	echo -e "Running treePL for the main species tree (${tpltree})...\n"
+	chmod 755 treepl_wrapper.sh
+	./treepl_wrapper.sh configuration.txt ${tplt} treePLresult > treePL_${tpltree}.log
+	#Modify names
+	mv treepl_treePLresult.tre ${tpltree}_treePLresult.tre
+	mv treepl_treePLresult.tre.r8s ${tpltree}_treePLresult.tre.r8s
+	mv out_dates.tre.r8s ${tpltree}_dates.tre.r8s
+	mv out_dates.tre ${tpltree}_dates.tre
+	#Delete some files
+	rm ${tplt} treepl_wrapper.sh
+	# Copy results to home
+	if [[ $update =~ "yes" ]]; then
+		cp * $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${tpltf}/treePL
+		cp treePL_${tpltree}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${tpltf}/treePL
+	else
+		cp * $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}${tpltf}/treePL
+		cp treePL_${tpltree}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}${tpltf}/treePL
 	fi
 fi
+
+#Finish log & copy home
+echo -e "\nEnd:" `date '+%A %d-%m-%Y %X'` >> ${logname}.log
+if [[ $update =~ "yes" ]]; then
+	cp ${logname}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${tpltf}/treePL
+else
+	cp ${logname}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/${modif}${tpltf}/treePL
+fi
+
 #Clean scratch/work directory
 if [[ $PBS_O_HOST == *".cz" ]]; then
 	#delete scratch
@@ -343,4 +463,7 @@ else
 	rm -r workdir14
 fi
 
+if [[ $tplbs =~ "yes" ]]; then
+	echo -e "Go to '$path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/${modif}${tpltf}/treePL/bstrees' and run 'submitTreePLjobs.sh'\n"
+fi
 echo -e "\nScript HybPhyloMaker14 finished...\n"
