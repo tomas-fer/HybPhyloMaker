b00d79e9013e49ccbf78b215b345604db064d21a
Tomas Fer <tomas.fer@centrum.cz>
2025-04-15 17:40:45 +0200

    v.1.8.0a
    add missing data handling (correct output after SNP-sites)

--- HybPhyloMaker8n_SNPheatmap.sh
+++ HybPhyloMaker8n_SNPheatmap.sh
@@ -19,7 +19,7 @@
 # *    HybPhyloMaker - Pipeline for Hyb-Seq data processing and tree building    *
 # *                  https://github.com/tomas-fer/HybPhyloMaker                  *
 # *                           Script 08n - SNP heatmap                           *
-# *                                   v.1.8.0                                    *
+# *                                   v.1.8.0a                                   *
 # *                                  Tomas Fer                                   *
 # *       Dept. of Botany, Charles University, Prague, Czech Republic, 2025      *
 # *                           tomas.fer@natur.cuni.cz                            *
@@ -138,6 +138,20 @@ if [[ ! $location == "1" ]]; then
 	fi
 fi
 
+#Check if 'snpmiss' is integer
+if [[ ! "$snpmiss" =~ ^[0-9]+$ ]]; then
+	echo -e "Variable 'snpmiss' is not correctly set:\nNow: $snpmiss\nShould be an integer between 0 and 100.\n Exiting...\n"
+	rm -d ../workdir08n 2>/dev/null
+	exit 3
+else
+	snpmiss=$((10#${snpmiss}))
+	if [[ "$snpmiss" -gt 100 ]]; then
+		echo -e "Variable 'snpmiss' is not correctly set:\nNow: $snpmiss\nShould be an integer between 0 and 100.\n Exiting...\n"
+		rm -d ../workdir08n 2>/dev/null
+		exit 3
+	fi
+fi
+
 #Write log
 logname=HPM8n
 echo -e "HybPhyloMaker8n: SNP heatmap" > ${logname}.log
@@ -163,7 +177,7 @@ echo -e "\nSettings" >> ${logname}.log
 if [[ $PBS_O_HOST == *".cz" ]]; then
 	printf "%-25s %s\n" `echo -e "\nServer:\t$server"` >> ${logname}.log
 fi
-for set in data selection cp corrected update tree OUTGROUP SNPs; do
+for set in data selection cp corrected update tree OUTGROUP SNPs snpmiss; do
 	printf "%-25s %s\n" `echo -e "${set}:\t" ${!set}` >> ${logname}.log
 done
 if [ ! -z "$selection" ]; then
@@ -220,29 +234,73 @@ sed -i 's/\?/N/g' concatenated.fasta
 #replace 'N's by '-'
 sed -i '/^>/!s/N/-/g' concatenated.fasta
 
-echo -e "Running SNP-site...\n"
-if [[ $PBS_O_HOST == *".cz" ]]; then
-	mamba activate SNP-sites
-	snp-sites -v concatenated.fasta > concatenated.vcf
-	mamba deactivate
-	module unload mambaforge
+if [[ $snpmiss -eq 0 ]]; then
+	echo -e "Running standard SNP-site...\n"
+	if [[ $PBS_O_HOST == *".cz" ]]; then
+		mamba activate SNP-sites
+		snp-sites -v concatenated.fasta > concatenated.vcf
+		mamba deactivate
+		module unload mambaforge
+	else
+		snp-sites -v concatenated.fasta > concatenated.vcf
+	fi
+	#Filter VCF
+	echo -e "Filtering VCF...\n"
+	gzip concatenated.vcf
+	bcftools view -e 'AC==0 || AC==AN || F_MISSING > 0.2' -m2 -M2 -O z -o concatenated_bcf.vcf.gz concatenated.vcf.gz
+	gzip -d -c concatenated_bcf.vcf.gz > concatenated_bcf.vcf
+	mv concatenated_bcf.vcf.gz FILTEREDconcatenated_${snpmiss}missing.vcf.gz
+	mv concatenated.vcf.gz FULLconcatenated_${snpmiss}missing.vcf.gz
+	mv concatenated_bcf.vcf FILTEREDconcatenated_${snpmiss}missing.vcf
+	mkdir 
 else
-	snp-sites -v concatenated.fasta > concatenated.vcf
+	#change alignment gaps ('-') to 'X'
+	echo -e "Running SNP-site with gaps changed to 'X'...\n"
+	sed '/^>/! s/-/X/g' concatenated.fasta > concatenatedModif.fasta
+	if [[ $PBS_O_HOST == *".cz" ]]; then
+		mamba activate SNP-sites
+		snp-sites -v concatenatedModif.fasta > concatenatedModif.vcf
+		mamba deactivate
+		module unload mambaforge
+	else
+		snp-sites -v concatenatedModif.fasta > concatenatedModif.vcf
+	fi
+	#separate header and data
+	grep "^#" concatenatedModif.vcf > header.txt
+	grep -v "^#" concatenatedModif.vcf > data.txt
+	#remove triallelic/tetraallelic SNPs
+	grep -v "[ACTGX],[ACTGX],[ACTGX]" data.txt > dataToModif.txt
+	#Modification commands - #explanation:
+	#-F"\" - works on TABs as column separators
+	#always checks value in 5th colum ($5), i.e. ALT
+	#if $5=X then set the value to "*" (i.e. indel) and then checks the values in columns from 10 until the end (for (i=10; i<=NF; i++)) - i.e. genotype values - and change all "1"s to "." (i.e. missing)
+	#than it checks whether $5 is [ACTG],X - i.e. two alternate alleles, the second 'X' - if yes it changes: (1) $5 to everything before ',' (gsub(/,.*/,"",$5) and (2) all genotype "2" to "." (i.e. missing)
+	#than it checks whether $5 is X,[ACTG] - i.e. two alternate alleles, the first 'X' - if yes it changes: (1) $5 to everything after ',' (gsub(/.*,/,"",$5), (2) all genotypes "1" to "." (i.e. missing) and (3) all genotypes "2" to "1"
+	#between every command there is sed command replacing ' ' to TABs
+	#FIRST - this treats 'X' in $5 (i.e. ALT)
+	awk -F"\t" '{ if ($5=="X") {$5="*";for (i=10; i<=NF; i++) if ($i=="1") {$i="."}; print $0} else {print $0} }' dataToModif.txt | sed "s/ /\t/g" | awk -F"\t" 'match($5, /[ACTG],X/) {gsub(/,.*/,"",$5);for (i=10; i<=NF; i++) if ($i=="2") {$i="."}; print $0; next}1' | sed "s/ /\t/g" | awk -F"\t" 'match($5, /X,[ACTG]/) {gsub(/.*,/,"",$5);for (i=10; i<=NF; i++) if ($i=="1") {$i="."}; for (i=10; i<=NF; i++) if ($i=="2") {$i="1"}; print $0; next}1' | sed "s/ /\t/g" > FIRSTdataModif.txt
+	#SECOND - this treats 'X' in $4 (i.e. REF)
+	#check if there is 'X' in $$ and two alternatives in $5 - then it put first alternative to 'a' and second alternative to 'b' and rename $4 and $5 by 'a' and 'b', respectively
+	#then it checks the values in columns from 10 until the end (for (i=10; i<=NF; i++)) - i.e. genotype values - and change all "0"s to "." (i.e. missing), all "1"s to "0"s and all "2"s to "1"s
+	#next awk command checks if there is 'X' in $4 and names $4 by '*'
+	awk -F"\t" '{if ($5~/[ACTG],[ACTG]/ && $4=="X") {a=(sprintf(substr($5,1,1)));b=(sprintf(substr($5,3,1))); $4=a; $5=b; for (i=10; i<=NF; i++) if ($i=="0") {$i="."}; for (i=10; i<=NF; i++) if ($i=="1") {$i="0"}; for (i=10; i<=NF; i++) if ($i=="2") {$i="1"}; print $0; } else {print $0} }' FIRSTdataModif.txt | sed "s/ /\t/g" | awk -F"\t" '{ if ($4=="X") {$4="*";for (i=10; i<=NF; i++) if ($i=="0") {$i="."}; print $0} else {print $0} }' | sed "s/ /\t/g" > SECONDdataModif.txt
+	#remove unvariable sites (with '*', i.e. sites where the only variability is missing site)
+	grep -v "*" SECONDdataModif.txt > FINALdataModif.txt
+	#combine with header
+	cat header.txt FINALdataModif.txt > concatenatedModifFinal.vcf
+	rm header.txt data.txt dataToModif.txt FIRSTdataModif.txt SECONDdataModif.txt FINALdataModif.txt
+	#filter the final VCF using BCFtools
+	gzip concatenatedModifFinal.vcf
+	#divide 'snpmiss' by '100' and filter with bcftools
+	fltr=`echo "scale=2; $snpmiss / 100" | bc | sed 's/^\./0./'`
+	bcftools view -e "AC==0 || AC==AN || F_MISSING > $fltr" -m2 -M2 -O z -o FILTEREDconcatenated_${snpmiss}missing.vcf.gz concatenatedModifFinal.vcf.gz
 fi
 
-#Filter VCF
-echo -e "Filtering VCF...\n"
-gzip concatenated.vcf
-bcftools view -e 'AC==0 || AC==AN || F_MISSING > 0.2' -m2 -M2 -O z -o concatenated_bcf.vcf.gz concatenated.vcf.gz
-gzip -d -c concatenated_bcf.vcf.gz > concatenated_bcf.vcf
-mv concatenated_bcf.vcf.gz FILTEREDconcatenated.vcf.gz
-mv concatenated.vcf.gz FULLconcatenated.vcf.gz
-mv concatenated_bcf.vcf FILTEREDconcatenated.vcf
-
+#CONTINUE HERE !!!
 #Create 0/1 matrix
 echo -e "Creating 0/1 matrix...\n"
 #only take columns 10+ (i.e., genotype data only)
-grep -v "^##" FILTEREDconcatenated.vcf | cut -f2,10- > SNPmatrix.txt
+grep -v "^##" FILTEREDconcatenated_${snpmiss}missing.vcf | cut -f2,10- > SNPmatrix.txt
 #transpose the matrix (lines will be samples)
 awk '
 {
@@ -262,29 +320,43 @@ END {
 }' SNPmatrix.txt > SNPmatrixTransposed.txt
 
 #Plot heatmap in R
-echo -e "Plotting heatmap in R...\n"
+echo -en "Plotting heatmap in R..."
 if [[ $PBS_O_HOST == *".cz" ]]; then
 	module unload python/3.7.7-gcc-8.3.0-t4loj4a #to avoid conflict with R4.4 (requires python3.9.12)
 	module add r/4.4.0-gcc-10.2.1-ssuwpvb
 	export R_LIBS="/storage/$server/home/$LOGNAME/Rpackages44"
 fi
-R --slave -f SNPheatmap.R >> R.log 2>&1
-mv SNPheatmap.pdf ${selection}_SNPheatmap.pdf
-mv distmat.txt ${selection}_SNPdistmat.txt
+if [[ $snpmiss -eq 0 ]]; then
+	echo -e "using DistBinary(PERMANOVA) - no missing data\n"
+	R --slave -f SNPheatmap.R 0 >> R.log 2>&1
+else
+	echo -e "using custom R function - missing data allowed\n"
+	R --slave -f SNPheatmap.R 1 >> R.log 2>&1
+fi
+
+if [ ! -z "$selection" ]; then
+	mv SNPheatmap.pdf ${selection}_SNPheatmap_${snpmiss}missing.pdf
+	mv distmat.txt ${selection}_SNPdistmat_${snpmiss}missing.txt
+else
+	mv SNPheatmap.pdf ${data}_SNPheatmap_${snpmiss}missing.pdf
+	mv distmat.txt ${data}_SNPdistmat_${snpmiss}missing.txt
+fi
 
 #Copy results to home
 if [[ $update =~ "yes" ]]; then
-	cp *.{pdf,gz,fasta,txt,log} $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/SNPheatmap
+	mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/SNPheatmap/${snpmiss}missing
+	cp *.{pdf,gz,fasta,txt,log} $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/SNPheatmap${snpmiss}missing
 else
-	cp *.{pdf,gz,fasta,txt,log} $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/SNPheatmap
+	mkdir $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/SNPheatmap/${snpmiss}missing
+	cp *.{pdf,gz,fasta,txt,log} $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/SNPheatmap/${snpmiss}missing
 fi
 
 #Copy log to home
 echo -e "\nEnd:" `date '+%A %d-%m-%Y %X'` >> ${logname}.log
 if [[ $update =~ "yes" ]]; then
-	cp ${logname}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/SNPheatmap
+	cp ${logname}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/update/species_trees/SNPheatmap/${snpmiss}missing
 else
-	cp ${logname}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/SNPheatmap
+	cp ${logname}.log $path/${treepath}${MISSINGPERCENT}_${SPECIESPRESENCE}/${tree}/species_trees/SNPheatmap/${snpmiss}missing
 fi
 
 #Clean scratch/work directory
